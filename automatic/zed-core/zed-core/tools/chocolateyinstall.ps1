10ba964d2ae449eda5487667f874065d44db74d7
Victor Sanikovich <sanikovich@gmail.com>
2026-06-24 22:40:19 +0300

    Initial commit: zed-core v1.8.2

---
+++ automatic/zed-core/zed-core/tools/chocolateyinstall.ps1
@@ -0,0 +1,19 @@
+﻿$ErrorActionPreference = 'Stop'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$packageName = 'zed-core'
+$softwareName = 'Zed'
+
+$url = 'https://github.com/zed-industries/zed/releases/download/v1.8.2/Zed-x86_64.exe'
+
+$packageArgs = @{
+  packageName   = $packageName
+  fileType      = 'exe'
+  url           = $url
+  softwareName  = $softwareName
+  checksum      = '147744BA96840924606345D4E7417FC53DF77A5FB809A6BECE053037378A2FD3'
+  checksumType  = 'sha256'
+  silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'  
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ automatic/zed-core/zed-core/tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName = 'zed-core'
+$softwareName = 'Zed'
+
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -eq 1) {
+    $uninstallString = $key[0].UninstallString
+    $uninstallArgs = @{
+        packageName    = $packageName
+        fileType       = 'exe'
+        silentArgs     = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+        validExitCodes = @(0)
+        file           = $uninstallString.Trim('"')
+    }
+    Uninstall-ChocolateyPackage @uninstallArgs
+} elseif ($key.Count -eq 0) {
+    Write-Warning "$packageName has already been uninstalled by other means."
+} else {
+    Write-Warning "$($key.Count) matches found! To prevent accidental data loss, no programs will be uninstalled."
+}
